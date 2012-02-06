78684c30dc36d03937cff224f5458496cd01349a
Vincent <cool8511@gmail.com>
2012-02-06 21:58:43 +0800

    inline FastBuffer short function to speed up. 2, auto shrink buffer size while free

--- Core/FastBuffer.cpp
+++ Core/FastBuffer.cpp
@@ -1,12 +1,16 @@
 #include "FastBuffer.h"
 #include <assert.h>
+#include <stdio.h>
 
 #define DEFAULT_SIZE 128
+#define SHRINK_SIZE  1024*DEFAULT_SIZE
+
 namespace core { namespace net {
 
 FastBuffer::FastBuffer()
     :m_readstart(0),
-    m_writestart(0)
+    m_writestart(0),
+    m_halfcounter(0)
 {
     m_innerdata.resize(DEFAULT_SIZE, 0);
 }
@@ -16,28 +20,33 @@ FastBuffer::~FastBuffer()
     clear();
 }
 
-const char* FastBuffer::data()
-{
-    assert(m_readstart <= m_innerdata.size());
-    return &*m_innerdata.begin() + m_readstart;
-}
-
-// the valid offset of char* returned by data()
-size_t FastBuffer::size() const
-{
-    assert(m_writestart <= m_readstart);
-    return m_writestart - m_readstart;
-}
-
-bool FastBuffer::empty() const
-{
-    return m_readstart == m_writestart;
-}
-
 void FastBuffer::push_back(const char* pdata, size_t datasize)
 {
     if(datasize == 0)
         return;
+    if(m_innerdata.size() > SHRINK_SIZE)
+    {
+        // auto shrink if there is much free space for a long time.
+        if(m_innerdata.size() - size() - datasize > SHRINK_SIZE/8*7)
+        {
+            if(m_halfcounter++ > 10)
+            {
+                printf("shrinking fastbuffer :%zu, used:%zu\n", m_innerdata.size(), size());
+                m_halfcounter = 0;
+                if(m_readstart > 0)
+                {
+                    std::copy(m_innerdata.begin() + m_readstart, m_innerdata.begin() + size(), m_innerdata.begin());
+                }
+                m_writestart = size();
+                m_readstart = 0;
+                m_innerdata.resize(m_innerdata.size()/2);
+            }
+        }
+        else
+        {
+            m_halfcounter = 0;
+        }
+    }
     assert(m_writestart <= m_innerdata.size());
     if(m_innerdata.size() - m_writestart < datasize)
     {
@@ -47,22 +56,6 @@ void FastBuffer::push_back(const char* pdata, size_t datasize)
     }
     std::copy(pdata, pdata + datasize, m_innerdata.begin() + m_writestart);
     m_writestart += datasize;
-}
-
-void FastBuffer::pop_front(size_t datasize)
-{
-    assert(datasize <= size());
-    m_readstart += datasize;
-    if(m_writestart <= m_readstart)
-    {
-        // no valid data in the vector, we can move the index to the begin to free space
-        m_readstart = m_writestart = 0;
-    }
-}
-
-void FastBuffer::clear()
-{
-    m_writestart = m_readstart = 0;
 }
 
 } }
--- Core/FastBuffer.h
+++ Core/FastBuffer.h
@@ -2,6 +2,7 @@
 #define CORE_NET_FASTBUFFER_H
 
 #include <vector>
+#include <assert.h>
 
 namespace core { namespace net {
 class FastBuffer
@@ -9,12 +10,37 @@ class FastBuffer
 public:
     FastBuffer();
     ~FastBuffer();
-    const char* data();
-    size_t size() const;
-    void  push_back(const char * pdata, size_t size);
-    bool  empty() const;
-    void  pop_front(size_t size);
-    void  clear();
+    inline const char* data()
+    {
+        assert(m_readstart <= m_innerdata.size());
+        return &*m_innerdata.begin() + m_readstart;
+    }
+    // the valid offset of char* returned by data()
+    inline size_t size() const
+    {
+        assert(m_writestart <= m_readstart);
+        return m_writestart - m_readstart;
+    }
+    void  push_back(const char * pdata, size_t datasize);
+    inline bool  empty() const
+    {
+        return m_readstart == m_writestart;
+    }
+    inline void  pop_front(size_t datasize)
+    {
+        assert(datasize <= size());
+        m_readstart += datasize;
+        if(m_writestart <= m_readstart)
+        {
+            // no valid data in the vector, we can move the index to the begin to free space
+            m_readstart = m_writestart = 0;
+        }
+    }
+    inline void  clear()
+    {
+        m_writestart = m_readstart = 0;
+    }
+
 private:
     int  m_readstart;
     int  m_writestart;
